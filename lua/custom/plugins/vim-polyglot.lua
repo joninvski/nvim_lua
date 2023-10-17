4db7bc586b88fecb56cbd6fde6709e5aeb874628
Joao Trindade <trindade.joao@gmail.com>
2023-10-17 23:12:58 +0100

    Disable sluth polyglot

--- lua/custom/plugins/vim-polyglot.lua
+++ lua/custom/plugins/vim-polyglot.lua
@@ -1,5 +1,10 @@
+vim.g.polyglot_disabled = {'autoindent'}
+
 return {
   'sheerun/vim-polyglot',
   version = "*",
   lazy = false,
+  -- config = function()
+  --   vim.g.polyglot_disabled = {'autoindent'}
+  -- end
 }
