5717fdab45bfcef7c654f809d7d15c5c9e574f9a
Joao Trindade <trindade.joao@gmail.com>
2023-10-18 15:41:08 +0100

    Fix lualine config

--- init.lua
+++ init.lua
@@ -171,12 +171,20 @@ require('lazy').setup({
       },
       sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_b = {},
         lualine_c = {{'filename', path = 1}},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {'encoding', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
-      }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
     },
   },
 
